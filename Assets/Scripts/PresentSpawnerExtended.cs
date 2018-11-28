44d005da524c26673d882dc78144aee52ad6cab3
barneynicholls <barney.nicholls@bluetoad.net>
2018-11-28 22:44:25 +0000

    adding asset store items for terrain build

---
+++ Assets/Scripts/PresentSpawnerExtended.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresentSpawnerExtended : MonoBehaviour {
+
+	public float distance = 2.0f;
+	public float maxInterval = 1.0f;
+	public float minInterval = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(presentSpawn());
+
+	}
+
+	IEnumerator presentSpawn() {
+		yield return new WaitForSeconds(Random.Range(minInterval,maxInterval));
+		var pos = gameObject.transform.position;
+		pos.x += Random.Range(distance * -1, distance);
+		pos.z += Random.Range(distance * -1, distance);
+		Instantiate(Resources.Load("present"),pos,Quaternion.identity);
+
+		StartCoroutine(presentSpawn());
+
+	}
+	
+}
