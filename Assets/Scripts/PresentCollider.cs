7ca1eb83f05924df8f481e4e153fad19778135e1
Barney Nicholls <barney.nicholls@headforwards.com>
2018-12-04 14:10:16 +0000

    end of lunch tuesday

---
+++ Assets/Scripts/PresentCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresentCollider : MonoBehaviour {
+
+void OnCollisionEnter(Collision collision)
+    {
+        var present = collision.gameObject;
+
+        // when collide destroy the pumpkin.
+        if (present.name.ToLower().StartsWith("present"))
+        {
+    //        Instantiate(Resources.Load("splat"), present.transform.position,Quaternion.identity);
+            Destroy(present);
+
+           EventManager.TriggerEvent("scoreupdate", gameObject.name);
+        }
+    }      
+	
+}
---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour {
+
+
+public TextMesh playerOneScore;
+	void Start () {
+		
+		playerOneScore.text = "score1";
+	}
+	
+}
